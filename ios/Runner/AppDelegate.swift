a109534740d83f003c85d3525d55488413237aa1
chetan <chetan.bhalerao@go-jek.com>
2018-07-30 19:58:07 +0530

    opened ios viewcontroller from channel message from flutter stateful widget component

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,21 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController;
+    let controllerChannel = FlutterMethodChannel.init(name: "test_activity",
+                                                   binaryMessenger: controller);
+    controllerChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        self.openViewController()
+    });
+    //GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    func openViewController(){
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let secondViewController = SecondViewController()
+        window?.rootViewController = UINavigationController(rootViewController: secondViewController)
+    }
 }
