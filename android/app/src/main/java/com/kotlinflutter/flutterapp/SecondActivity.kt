bf1e73982bc34f1f912b8992db776626b51ba8d2
chetan <chetan.bhalerao@go-jek.com>
2018-08-05 10:41:21 +0530

    fixed some design on android screen

--- android/app/src/main/java/com/kotlinflutter/flutterapp/SecondActivity.kt
+++ android/app/src/main/java/com/kotlinflutter/flutterapp/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.kotlinflutter.flutterapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 
 class SecondActivity : AppCompatActivity() {
@@ -9,8 +10,21 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        setSupportActionBar(toolbar)
 
+        // Now get the support action bar
+        val actionBar = supportActionBar
 
+        // Set toolbar title/app title
+        actionBar!!.title = "Android Component"
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,11 @@ class FlutterComponent extends State<FlutterPage> {
             padding: EdgeInsets.all(16.0),
             child: new Column(
               children: <Widget>[
+                new Text('Flutter Component',
+                  style: TextStyle(fontSize: 20.0, color: Colors.blue),
+                  textAlign: TextAlign.center,
+                ),
+                new SizedBox(height: 16.0,),
                 new MaterialButton(
                     child: const Text('Open Screen'),
                     elevation: 5.0,
@@ -68,67 +73,3 @@ class FlutterComponent extends State<FlutterPage> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const String _channel = 'increment';
-  static const String _pong = 'pong';
-  static const String _emptyMessage = '';
-  static const BasicMessageChannel<String> platform =
-      const BasicMessageChannel<String>(_channel, const StringCodec());
-
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    platform.setMessageHandler(_handlePlatformIncrement);
-  }
-
-  Future<String> _handlePlatformIncrement(String message) async {
-    setState(() {
-      _counter++;
-    });
-    return _emptyMessage;
-  }
-
-  void _sendFlutterIncrement() {
-    platform.send(_pong);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Expanded(
-            child: new Center(
-                child: new Text(
-                    'Platform button tapped $_counter time${ _counter == 1
-                        ? ''
-                        : 's' }.',
-                    style: const TextStyle(fontSize: 17.0))),
-          ),
-          new Container(
-            padding: const EdgeInsets.only(bottom: 15.0, left: 5.0),
-            child: new Row(
-              children: <Widget>[
-                new Image.asset('assets/flutter-mark-square-64.png',
-                    scale: 1.5),
-                const Text('Flutter', style: const TextStyle(fontSize: 30.0)),
-              ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _sendFlutterIncrement,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
